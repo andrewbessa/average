1630f9f3f5fb6a047ba5f3f913fa857ed88e86a4
andrewbessa <iniciativaandrebessa@gmail.com>
2019-10-14 00:20:57 -0300

    update methods name

--- average/average_calc.py
+++ average/average_calc.py
@@ -4,13 +4,13 @@ from datetime import timedelta
 class AverageCalc:
 
     def __init__(self, window_size):
-        self.__window_size = window_size
         self.__timestamp_duration_list =[]
         self.__delta_increment = timedelta(minutes=1)
         self.__delta_window = timedelta(minutes=int(window_size))
         self.__overlap_list = []
     
-    def add_tranlation_delivered(self, tranlation_delivered):   
+    def add_tranlation_delivered(self, tranlation_delivered):
+        """Record the translation delivered events"""   
         button = self.__get_time_until_minutes(tranlation_delivered.timestamp) + self.__delta_increment
         top = button + self.__delta_window
         self.__timestamp_duration_list.append((button, top, tranlation_delivered.duration))
@@ -18,17 +18,18 @@ class AverageCalc:
     def cal_avg_delivered_time(self):
         if len(self.__timestamp_duration_list) > 0:
 
-            reference = self.__get_time_until_minutes(self.__timestamp_duration_list[0][0]) - self.__delta_increment
+            time_reference = self.__get_time_until_minutes(self.__timestamp_duration_list[0][0]) - self.__delta_increment
             
             while len(self.__timestamp_duration_list) > 0:
-                self.__update_overlap_list(reference)
+                self.__update_overlap_list(time_reference)
                 self.__update_timestamp_duration_list()
 
-                yield (str(reference), self.__calc_overlap_avg())                
+                yield (str(time_reference), self.__calc_overlap_average())                
                 
-                reference += self.__delta_increment
+                time_reference += self.__delta_increment
 
     def __update_overlap_list(self, reference):
+        """Keep the list with events in the average time window"""
         self.__remove_element_out_of_range(reference)
 
         for item in self.__timestamp_duration_list:
@@ -49,19 +50,19 @@ class AverageCalc:
             if item in self.__timestamp_duration_list:
                 self.__timestamp_duration_list.remove(item)
 
-    def __calc_overlap_avg(self):
-        avg = 0
+    def __calc_overlap_average(self):
+        average = 0
         if len(self.__overlap_list) > 0:
             size = len(self.__overlap_list)
             amount = 0
             for item in self.__overlap_list:
                 if item[0] < item[1]:
                     amount += item[2]
-            avg = amount/size
-            if int(avg) == avg:
-                avg = int(avg)
+            average = amount/size
+            if int(average) == average:
+                average = int(average)
                 
-        return avg
+        return average
    
     def __get_time_until_minutes(self, timestamp):
         return  datetime(year=timestamp.year,
